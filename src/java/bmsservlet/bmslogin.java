5968bf844028ca661da7f002089fb34100ac6852
Kartik <kartikkhurani99@gmail.com>
2018-11-13 22:05:02 +0530

    BMS

---
+++ src/java/bmsservlet/bmslogin.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bmsservlet;
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author KARTIK
+ */
+public class bmslogin extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+        Class.forName("com.mysql.cj.jdbc.Driver");   
+           Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb?useSSL=false","root","iamkartik18");
+           
+           PreparedStatement pstmt=conn.prepareStatement("insert into bmslogin values(?,?,?,?,?,?)");
+           String s1 = request.getParameter("fname");
+           String s2 = request.getParameter("lname");
+           String s3 = request.getParameter("pw");
+           String s4 = request.getParameter("day");
+           String s5 = request.getParameter("month");
+           String s6 = request.getParameter("year");
+           String s7 = request.getParameter("gender");
+           //System.out.println(s7);
+           String s8 = request.getParameter("email");
+           
+           pstmt.setString(1,s1);
+           pstmt.setString(2,s2);
+           pstmt.setString(3,s3);
+           pstmt.setString(4,s4+"/"+s5+"/"+s6);
+           pstmt.setString(5,s7);
+           pstmt.setString(6,s8);
+           pstmt.executeUpdate();
+        }
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
